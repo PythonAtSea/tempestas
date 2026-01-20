558819e7833b37a1e9ba34c18724f06e91f8f4c6
PythonAtSea <HudsonGreen27@gmail.com>
2026-01-20 09:33:35 -0500

    fix: stuff

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   env: {
-    NEXT_PUBLIC_COMMIT_SHA: process.env.CF_PAGES_COMMIT_SHA || "",
+    NEXT_PUBLIC_COMMIT_SHA: process.env.WORKERS_CI_COMMIT_SHA || "",
   },
 };
 
